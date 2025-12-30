7e53fe3f19a9fd05584720f47472c129711c7ce9
bodakaveri02 <bodakaveri02@gmail.com>
2025-12-30 11:18:27 +0530

    Update Calculator.py
    BY using python built-in GUI library, created a simple calculator.

--- Calculator.py
+++ Calculator.py
@@ -11,14 +11,14 @@ class Calculator:
     def create_display(self):
         frame=tk.Frame()
         frame.pack()
-        entry=tk.Entry(frame,font=("Arial",18),justify="left")
+        entry=tk.Entry(frame,textvariable=self.input_text,font=("Arial",18),justify="right")
         entry.pack()
     def create_Buttons(self):
         frame=tk.Frame()
         frame.pack()
         buttons=[('7',1,0),('8',1,1),('9',1,2),('/',1,3),
-                 ('4',2,0),('5',2,1),('6',2,2),('*',2,3)
-                 ('1',3,0),('2',3,1),('3',3,2),('-',3,3)
+                 ('4',2,0),('5',2,1),('6',2,2),('*',2,3),
+                 ('1',3,0),('2',3,1),('3',3,2),('-',3,3),
                  ('0',4,0),('.',4,1),('=',4,2),('+',4,3),]
         for text,row,col in buttons:
             if text=='=':
@@ -26,7 +26,7 @@ class Calculator:
             else:
                 btn=tk.Button(frame,text=text,width=6,height=2,command=lambda t=text:self.press(t))
             btn.grid(row=row,column=col)
-        tk.Button(frame,text="C",width=26,height=2,command=self.clear).grid(row=5,column=0,columnspa)
+        tk.Button(frame,text="C",width=26,height=2,command=self.clear).grid(row=5,column=0,columnspan=4)
     def press(self,value):
         self.expression+=(value)
         self.input_text.set(self.expression)
@@ -35,6 +35,12 @@ class Calculator:
             result=str(eval(self.expression))
             self.expression=result
             self.input_text.set(result)
+        except:
+            self.input_text.set("Error")
+            self.expression=""
+    def clear(self):
+        self.input_text.set("")
+        self.expression=""
 
 root=tk.Tk()
 Calculator(root)
